26fb84f247fd814209fec72ac9f3d666d7a0aca3
brandonlira <brandonlira98@gmail.com>
2026-04-07 11:41:04 -0300

    ZIMA: Auth keys

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '9e7831ce3bf875cd';
+  public const string VERSIONS_HASH = '8c809ae781b12fce';
 
 }
